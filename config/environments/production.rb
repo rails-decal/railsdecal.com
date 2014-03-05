cfe4d94d094451ee7a9d17e21a01a7cff6f8f370
Mark Miyashita <negativetwelve@gmail.com>
2014-03-05 02:29:34 -0800

    Add Google Analytics to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ RailsDecal::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  GA.tracker = "UA-48665683-1"
 end
