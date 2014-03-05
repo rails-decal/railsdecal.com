3bba29c701d1e8088f2557c705947831140a2b88
Mark Miyashita <negativetwelve@gmail.com>
2014-03-05 01:12:28 -0800

    Add base Rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsDecal::Application.config.secret_key_base = '15bc9ab0578c3e6eedacfcd88724c31c1244212b456950c7a0039f9f6775229a7d4d7610d760bbdaead61119e58bc3555ff3b34af63209d28ad4214648994b80'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsDecal::Application.config.session_store :cookie_store, key: '_rails-decal_session'
