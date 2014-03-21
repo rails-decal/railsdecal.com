66717a348478ca3002334e7c673728f929b980dd
Mark Miyashita <negativetwelve@gmail.com>
2014-03-20 20:27:54 -0700

    Add active_li_link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def active_li_link(name=nil, options={}, html_options={}, link_html_options={}, &block)
+    html_options[:class] = "#{html_options[:class]} active" if current_page?(options)
+    content_tag :li, html_options do
+      link_to(name, options, link_html_options)
+    end
+  end
+
 end
