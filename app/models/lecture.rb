d020aaecc2571086cc2a4bbd9d7befbfb88b08c3
Mark Miyashita <negativetwelve@gmail.com>
2014-03-20 20:27:10 -0700

    Add method to group lectures together by year then by semester

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,7 +1,17 @@
 class Lecture < ActiveRecord::Base
 
+  Lecture.all.group_by(&:year).map{|year, lectures| { year => lectures.group_by{|l| l.semester}}}
+
   def filename
     "lectures/#{year}/#{semester.downcase}/#{partial.downcase}"
+  end
+
+  class << self
+
+    def by_year_and_semester
+      all.group_by(&:year).map{|year, lectures| {year => lectures.group_by{|lecture| lecture.semester}}}
+    end
+
   end
 
 end
