4ef98541d84f9abf97423dcea49960eda43568e5
Mark Miyashita <negativetwelve@gmail.com>
2014-04-09 15:55:26 -0700

    Add new applications form with fields

--- app/controllers/student_applications_controller.rb
+++ app/controllers/student_applications_controller.rb
@@ -1,6 +1,18 @@
 class StudentApplicationsController < ApplicationController
+  before_filter :student_application, only: [:create]
 
   def new
+    @student_application = StudentApplication.new
+  end
+
+  def create
+
+  end
+
+  private
+
+  def student_application
+    @student_application = StudentApplication.find(params[:id])
   end
 
 end
