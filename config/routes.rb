76d8be8830be1f82ecddd23f6cf5fe1e86e9a4ac
Mark Miyashita <negativetwelve@gmail.com>
2014-04-09 15:55:00 -0700

    Add post path for application

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ RailsDecal::Application.routes.draw do
   root to: "pages#home"
 
   match "apply", to: "student_applications#new", via: :get
+  match "apply", to: "student_applications#create", as: "student_applications", via: :post
 
   resources :lectures, only: [:show, :index]
 end
