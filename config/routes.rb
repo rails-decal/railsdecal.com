ade19f2db5b05fe618d29ed5bc8444c5d137ee15
Mark Miyashita <negativetwelve@gmail.com>
2014-04-09 15:40:55 -0700

    Add routes for student application

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ RailsDecal::Application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
+  match "apply", to: "student_applications#new", via: :get
+
   resources :lectures, only: [:show, :index]
 end
