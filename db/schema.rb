6f5a457c8439b8aa552555de87066d2e8e55682a
Mark Miyashita <negativetwelve@gmail.com>
2014-04-09 20:20:48 -0700

    Create roles model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140409223207) do
+ActiveRecord::Schema.define(version: 20140410031952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20140409223207) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "semester_id"
+  end
+
+  create_table "roles", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "semesters", force: true do |t|
@@ -63,6 +69,9 @@ ActiveRecord::Schema.define(version: 20140409223207) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "role_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
