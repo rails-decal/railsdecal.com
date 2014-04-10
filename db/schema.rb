89000a6e8225194df12f2d0633d08775fb341487
Mark Miyashita <negativetwelve@gmail.com>
2014-04-09 20:35:53 -0700

    Add name to positions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140410033150) do
+ActiveRecord::Schema.define(version: 20140410033531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140410033150) do
   create_table "positions", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   create_table "roles", force: true do |t|
