9bffccc817ffd10b396eb83bb25e739f8dfd4b5a
Mark Miyashita <negativetwelve@gmail.com>
2014-04-09 20:20:09 -0700

    Add columns to users

---
+++ db/migrate/20140410031816_add_columns_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :role_id, :integer
+  end
+end
