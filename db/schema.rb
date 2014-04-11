118c6f9cd088b577abde50467b693824baa0b40b
Mark Miyashita <negativetwelve@gmail.com>
2014-04-10 19:47:11 -0700

    Add more columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140410034224) do
+ActiveRecord::Schema.define(version: 20140411024516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,12 @@ ActiveRecord::Schema.define(version: 20140410034224) do
     t.string   "last_name"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
+    t.string   "nickname"
+    t.string   "image_url"
+    t.text     "bio"
+    t.string   "blog"
+    t.string   "location"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
