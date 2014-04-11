beb50fadd4d1b98953f043fb3c6d2751f24617fb
Mark Miyashita <negativetwelve@gmail.com>
2014-04-10 19:48:23 -0700

    Add oauth to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         :omniauthable, omniauth_providers: [:github]
 
   has_many :roles
 
@@ -12,6 +13,29 @@ class User < ActiveRecord::Base
 
   def is_staff?
     self.current_role.name == "Instructor" || self.current_role.name == "TA"
+  end
+
+  def self.find_for_github_oauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        user.name = auth.info.name
+        user.nickname = auth.info.nickname
+        user.bio = auth.extra.raw_info.bio
+        user.blog = auth.extra.raw_info.blog
+        user.location = auth.extra.raw_info.location
+        user.image_url = auth.info.image
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.github_data"] && session["devise.github_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
   end
 
 end
