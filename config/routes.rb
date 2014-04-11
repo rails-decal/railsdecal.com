65735623a376ef6cd80d2246e03f246ff20fe1df
Mark Miyashita <negativetwelve@gmail.com>
2014-04-10 20:12:05 -0700

    Add routes and omniauth initializers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 RailsDecal::Application.routes.draw do
-  devise_for :users
   root to: "pages#home"
 
   match "apply", to: "student_applications#new", via: :get
   match "apply", to: "student_applications#create", as: "student_applications", via: :post
 
   resources :lectures, only: [:show, :index]
+
+  devise_for :users, path: '',
+                     path_names: { sign_in: 'login', sign_up: 'sign-up', sign_out: 'logout'},
+                     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  resources :users, only: [:show]
 end
