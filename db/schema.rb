61bf15161efcb9e6314250a8036bb292fc9506cc
Mark Miyashita <negativetwelve@gmail.com>
2014-04-20 10:54:14 -0700

    Add semester to student applications

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140420041010) do
+ActiveRecord::Schema.define(version: 20140420174815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20140420041010) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "semester_id"
   end
 
   create_table "users", force: true do |t|
