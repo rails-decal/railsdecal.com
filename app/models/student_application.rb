ffc73c3378eb5e4ce2c564bcf372338f2bc6ca8f
Mark Miyashita <negativetwelve@gmail.com>
2014-04-19 20:15:37 -0700

    Add validations for student_application

--- app/models/student_application.rb
+++ app/models/student_application.rb
@@ -1,2 +1,6 @@
 class StudentApplication < ActiveRecord::Base
+
+  validates_presence_of :first_name, :last_name, :email, :year,
+                        :major, :why_join, :cs_classes_taken, :current_courseload,
+                        :other_commitments, :how_many_hours_willing, :how_did_you_hear_about_us
 end
