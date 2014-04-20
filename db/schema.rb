b484858f0dc10db095212e5c540c6f8872d06be8
Mark Miyashita <negativetwelve@gmail.com>
2014-04-19 21:17:53 -0700

    Add user association to student application

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140420032540) do
+ActiveRecord::Schema.define(version: 20140420041010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20140420032540) do
     t.text     "how_many_hours_willing"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
