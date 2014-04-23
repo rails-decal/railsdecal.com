07889d8439506f5ad7d5295272c0afc14e3165b0
Howard Chen <howardxchen@gmail.com>
2014-04-22 18:58:50 -0700

    Add another seed user

--- db/seeds/004_student_applications.rb
+++ db/seeds/004_student_applications.rb
@@ -11,3 +11,17 @@ app1 = StudentApplication.create(
   how_many_hours_willing: 'All day err day',
   how_did_you_hear_about_us: 'friendz'
 )
+
+app2 = StudentApplication.create(
+  first_name: 'Marky',
+  last_name: 'Mark',
+  email: 'mush@room.com',
+  year: 'Graduate',
+  major: 'ECCS',
+  why_join: 'To learn how trains work',
+  cs_classes_taken: 'None',
+  current_courseload: 'Grad Classez',
+  other_commitments: 'This git commitment',
+  how_many_hours_willing: '5 minutes',
+  how_did_you_hear_about_us: 'Google Search'
+)
