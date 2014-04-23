503ac3ed57e98d9589962f82d64ffda2316cc9ad
Howard Chen <howardxchen@gmail.com>
2014-04-22 18:56:19 -0700

    Add one seed application

---
+++ db/seeds/004_student_applications.rb
@@ -0,0 +1,13 @@
+app1 = StudentApplication.create(
+  first_name: 'Howie',
+  last_name: 'Bear',
+  email: 'yolo@swag.com',
+  year: 'Sophmore',
+  major: 'CS',
+  why_join: 'Idk',
+  cs_classes_taken: '61Swag',
+  current_courseload: '61 Yolo',
+  other_commitments: '61 Swag',
+  how_many_hours_willing: 'All day err day',
+  how_did_you_hear_about_us: 'friendz'
+)
