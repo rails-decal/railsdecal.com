90a65fdacf6899eeb7eebee4f89ec7b65a327efc
Mark Miyashita <negativetwelve@gmail.com>
2014-04-24 04:09:34 -0700

    Fix syntax error, only in development

--- db/seeds/005_users.rb
+++ db/seeds/005_users.rb
@@ -1,8 +1,10 @@
-(1..100).each do |index|
-  User.create(
-    email: "user#{index}@user.com",
-    password: "password",
-    name: "user#{index} user#{index}"
-    nickname: "user#{index}",
-  )
+if Rails.env.development?
+  (1..100).each do |index|
+    User.create(
+      email: "user#{index}@user.com",
+      password: "password",
+      name: "user#{index} user#{index}",
+      nickname: "user#{index}",
+    )
+  end
 end
