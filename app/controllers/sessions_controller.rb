f58e299496304f39cd47b5523892517fb6bec7ea
Mark Miyashita <negativetwelve@gmail.com>
2014-04-24 03:39:59 -0700

    Update sessions controller to use devise methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,42 @@
-class SessionsController < ApplicationController
+class SessionsController < Devise::SessionsController
 
   def new
+    redirect_to root_url if Rails.env.production?
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    respond_with(resource, serialize_options(resource))
   end
 
   def create_github
     user = User.from_omniauth(env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to root_url, notice: "Signed in."
+    redirect_to root_url, info: "Welcome back!"
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to root_url, notice: "Logged in!"
-    else
-      flash.now.alert = "Email or password is invalid."
-      redirect_to new_session_path, notice: 'Email or password is invalid'
-    end
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:info, :signed_in) if is_flashing_format?
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
   end
 
   def destroy
-    session[:user_id] = nil
-    redirect_to root_url, notice: "Logged out!"
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message :warning, :signed_out if signed_out && is_flashing_format?
+    yield if block_given?
+    respond_to_on_destroy
+  end
+
+  private
+
+  def respond_to_on_destroy
+    # We actually need to hardcode this as Rails default responder doesn't
+    # support returning empty response on GET request
+    respond_to do |format|
+      format.all { head :no_content }
+      format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
+    end
   end
 
 end
