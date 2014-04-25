e44c80bf62af62dd658d2a2de59fd5d25926f540
Mark Miyashita <negativetwelve@gmail.com>
2014-04-24 19:15:39 -0700

    Remove index on email for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140424101228) do
+ActiveRecord::Schema.define(version: 20140425021516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,7 +103,6 @@ ActiveRecord::Schema.define(version: 20140424101228) do
     t.boolean  "enabled",                default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
