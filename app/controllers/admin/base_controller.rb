d60fd6231848e5a0fc9ca40fdb24fa18204be026
Sam Lau <samlau95@gmail.com>
2014-04-28 03:27:43 -0700

    Authorize admin dashboard

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,10 +1,3 @@
 class Admin::BaseController < ApplicationController
-  before_action :admin_user
-
-  private
-
-  def admin_user
-    redirect_to(root_url) unless current_user && current_user.is_staff?
-  end
-
+  authorize_resource class: false
 end
