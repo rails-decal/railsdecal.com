a5e80fe9ea4b79f48746148cd6e690578a1b8c05
Sam Lau <samlau95@gmail.com>
2014-04-28 03:25:34 -0700

    Authorization for applying

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,13 +5,15 @@ class Ability
     user ||= User.new
 
     alias_action :index, :show, to: :view
+    alias_action :new, :create, to: :apply
 
     if user.is_current_staff?
       can :manage, :all
     elsif user.enabled?
       can :show, User, id: user.id
       can :view, Lecture
-    else
+      can :apply, StudentApplication if !user.submitted_current_semester_application?
+    else # not enabled or not signed in
     end
   end
 end
