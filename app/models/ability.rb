d341c194143a24b1fe3fce8c99ebe69e2af6da32
Sam Lau <samlau95@gmail.com>
2014-04-28 03:00:59 -0700

    Fix alias

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,12 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    alias_action [:index, :show], to: :view
+    alias_action :index, :show, to: :view
 
     if user.is_current_staff?
       can :manage, :all
     elsif user.enabled?
-      can :view, User, id: user.id
+      can :show, User, id: user.id
       can :view, Lecture
     else
     end
