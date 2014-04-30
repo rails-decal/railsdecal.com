7a92dca3257b1ab51798ce7763b10ff5fcd01191
Mark Miyashita <negativetwelve@gmail.com>
2014-04-30 10:55:05 -0700

    Load semester for evaluation methods

--- app/controllers/admin/evaluations_controller.rb
+++ app/controllers/admin/evaluations_controller.rb
@@ -1,6 +1,8 @@
 class Admin::EvaluationsController < Admin::BaseController
+  before_filter :semester
+
   def create
-    @student_application = StudentApplication.find(params[:student_application_id])
+    @student_application = StudentApplication.find(params[:id])
     @evaluation = Evaluation.find_by_user_id_and_student_application_id(current_user.id, @student_application.id) || Evaluation.new(evaluation_params)
     @evaluation.update(evaluation_params)
     @evaluation.student_application = @student_application
@@ -9,22 +11,26 @@ class Admin::EvaluationsController < Admin::BaseController
       if @student_application.next.nil?
         redirect_to admin_student_applications_path
       else
-        redirect_to admin_student_application_path(@student_application.next)
+        redirect_to admin_student_application_path(semester_url: @semester.url, id: @student_application.next.id)
       end
     else
-      redirect_to admin_student_application_path(@student_applicaiton)
+      redirect_to admin_student_application_path(semester_url: @semester.url, id: @student_applicaiton.id)
     end
   end
 
   def index
-    @semester = Semester.current
     @applications = StudentApplication.all
     @users = User.current_staff
   end
 
   private
 
+  def semester
+    @semester = Semester.find_by(url: params[:semester_url])
+  end
+
   def evaluation_params
     params.require(:evaluation).permit(:decision, :comment, :application_id)
   end
+
 end
