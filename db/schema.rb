022b04fbf38b90da8a8ae186b430ed85d328bf21
Sam Lau <samlau95@gmail.com>
2014-09-01 17:11:02 -0700

    Updates to home page

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140430171653) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "evaluations", force: true do |t|
     t.text     "decision"
     t.integer  "user_id"
