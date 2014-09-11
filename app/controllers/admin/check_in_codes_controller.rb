399459e5150e1cd6466c91b3e5ba802af2788fa1
Mark Miyashita <negativetwelve@gmail.com>
2014-09-10 23:18:46 -0700

    Form for creating check in codes

--- app/controllers/admin/check_in_codes_controller.rb
+++ app/controllers/admin/check_in_codes_controller.rb
@@ -1,6 +1,8 @@
 class Admin::CheckInCodesController < Admin::BaseController
   load_and_authorize_resource :check_in_code
 
+  require 'securerandom'
+
   def index
   end
 
@@ -8,9 +10,24 @@ class Admin::CheckInCodesController < Admin::BaseController
   end
 
   def new
+    @check_in_code.code = SecureRandom.hex(4)
   end
 
   def create
+    @check_in_code = CheckInCode.new(check_in_code_params)
+    if @check_in_code.save
+      flash[:info] = "Your check in code was created."
+      redirect_to admin_check_in_code_path(@check_in_code)
+    else
+      flash[:error] = "There was a problem creating the check in code."
+      render "new"
+    end
+  end
+
+  private
+
+  def check_in_code_params
+    params.require(:check_in_code).permit(:code, :class_date)
   end
 
 end
