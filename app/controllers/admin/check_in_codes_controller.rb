fc27477ac60bd76cc9fbe955c6a9d5247ebf4f08
Mark Miyashita <negativetwelve@gmail.com>
2014-09-10 23:07:47 -0700

    Add check in codes controller with basic blank methods

---
+++ app/controllers/admin/check_in_codes_controller.rb
@@ -0,0 +1,16 @@
+class Admin::CheckInCodesController < Admin::BaseController
+  load_and_authorize_resource :check_in_code
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+end
