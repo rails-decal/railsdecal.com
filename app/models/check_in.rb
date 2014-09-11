0f39c5919b3a9a8549033b65fb98d889cfa1f24b
Mark Miyashita <negativetwelve@gmail.com>
2014-09-10 23:02:55 -0700

    Annotate models

--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: check_ins
+#
+#  id               :integer          not null, primary key
+#  user_id          :integer
+#  check_in_code_id :integer
+#  created_at       :datetime
+#  updated_at       :datetime
+#
+
 class CheckIn < ActiveRecord::Base
   belongs_to :user
   belongs_to :check_in_code
--- spec/models/check_in_spec.rb
+++ spec/models/check_in_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: check_ins
+#
+#  id               :integer          not null, primary key
+#  user_id          :integer
+#  check_in_code_id :integer
+#  created_at       :datetime
+#  updated_at       :datetime
+#
+
 require 'spec_helper'
 
 describe CheckIn do
