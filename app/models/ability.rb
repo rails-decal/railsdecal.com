01430cf786db1c267d08ac4b1c7c564f5d4c9499
Howard Chen <howardxchen@gmail.com>
2015-05-08 03:05:00 -0700

    Temporarily disable enabling

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,11 +9,13 @@ class Ability
 
     can :create, CheckIn
     can :view, Lecture
+    can :show, User, id: user.id
 
     if user.is_current_staff?
       can :manage, :all
-    elsif user.enabled?
-      can :show, User, id: user.id
+    #Readd when enabled is added.
+    #elsif user.enabled?
+    #  can :show, User, id: user.id
     elsif !user.new_record? # signed in but not enabled
       can :apply, StudentApplication if !user.submitted_current_semester_application?
     end
