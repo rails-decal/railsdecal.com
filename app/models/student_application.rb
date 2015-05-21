df308907d7c41cf6cae5f3cce11393c8a886528c
Howard Chen <howardxchen@gmail.com>
2015-05-21 14:52:06 -0700

    Add standing to student application form

--- app/models/student_application.rb
+++ app/models/student_application.rb
@@ -30,7 +30,7 @@ class StudentApplication < ActiveRecord::Base
 
   has_many :evaluations, dependent: :destroy
 
-  validates_presence_of :first_name, :last_name, :email, :year,
+  validates_presence_of :first_name, :last_name, :email, :year, :standing,
                         :major, :why_join, :cs_classes_taken, :current_courseload,
                         :other_commitments, :how_many_hours_willing, :how_did_you_hear_about_us
 
