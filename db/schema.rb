b3a341a8418a04826887826e6eeeb1ba0920a9e9
Howard Chen <howardxchen@gmail.com>
2015-07-24 19:44:55 -0700

    Add limits to semesters

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710194918) do
+ActiveRecord::Schema.define(version: 20150725024418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,6 +101,8 @@ ActiveRecord::Schema.define(version: 20150710194918) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "url"
+    t.integer  "lower_division_limit"
+    t.integer  "upper_division_limit"
   end
 
   create_table "student_applications", force: :cascade do |t|
