2d74d9e0e6b4ce63a6c50ed9c25383c3fd4187a4
Howard Chen <howardxchen@gmail.com>
2015-07-28 21:53:09 -0700

    Add letter openner

--- app/jobs/acceptance_job.rb
+++ app/jobs/acceptance_job.rb
@@ -2,10 +2,9 @@ class AcceptanceJob < ActiveJob::Base
   queue_as :default
 
   def perform(student_application_id)
-    puts "Yay performed job"
-    AcceptanceMailer.accept(student_application_id).deliver_later
     student_application = StudentApplication.find(student_application_id)
     user = student_application.user
     user.update enabled: true if user
+    AcceptanceMailer.delay.accept(student_application_id)
   end
 end
