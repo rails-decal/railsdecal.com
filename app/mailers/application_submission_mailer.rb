9069a27b914a142856ebae5cfa2cec25ce14bef4
Howard Chen <howardxchen@gmail.com>
2015-08-08 16:39:20 -0700

    Remove extra puts

--- app/mailers/application_submission_mailer.rb
+++ app/mailers/application_submission_mailer.rb
@@ -2,7 +2,6 @@ class ApplicationSubmissionMailer < ApplicationMailer
   def send_confirmation(student_application_id)
     @student_application = StudentApplication.find(student_application_id)
     @semester = Semester.current
-    puts "Emplating"
     mail(to: @student_application.email, subject: "Rails Decal Application Received!")
   end
 end
