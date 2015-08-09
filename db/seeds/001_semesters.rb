be79d7e44bba3332ea4ceed9d1ebde8042747a85
Howard Chen <howardxchen@gmail.com>
2015-08-08 17:02:07 -0700

    Convert to Fall 2015

--- db/seeds/001_semesters.rb
+++ db/seeds/001_semesters.rb
@@ -1,2 +1,3 @@
 fall_2014 = Semester.where(semester: "Fall", year: 2014).first_or_create!
 spring_2015 = Semester.where(semester: "Spring", year: 2015).first_or_create!
+fall_2015 = Semester.where(semester: "Fall", year: 2015).first_or_create!
