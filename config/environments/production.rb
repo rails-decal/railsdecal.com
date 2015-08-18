26a6fc64462c56e9ee9e57a3fa27562ff5fec678
Howard Chen <howardxchen@gmail.com>
2015-08-17 22:39:12 -0700

    Fix gmail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ RailsDecal::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'railsdecal.com',
+    port:                 465,
+    domain:               'gmail.com',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
