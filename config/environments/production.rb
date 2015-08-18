f55d12ee972f6fe7b7227cad64d131399cfd1c95
Howard Chen <howardxchen@gmail.com>
2015-08-17 22:49:57 -0700

    Another gmail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ RailsDecal::Application.configure do
     domain:               'gmail.com',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'plain',
+    authentication:       'login',
     enable_starttls_auto: true  }
 
 end
