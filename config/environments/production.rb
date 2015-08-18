30b873838d1623d5698db0bd7106b80ce79413df
Howard Chen <howardxchen@gmail.com>
2015-08-17 22:58:03 -0700

    please work im tired

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,11 +84,11 @@ RailsDecal::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 465,
+    port:                 587,
     domain:               'gmail.com',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'login',
+    authentication:       'plain',
     enable_starttls_auto: true  }
 
 end
