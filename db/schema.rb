3d19bfbe8e9e03786d5163e16ea52fdc1af40515
Juno Student <j.morey@berkeley.edu>
2015-12-30 15:42:33 -0800

    add sid to student applications

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111014229) do
+ActiveRecord::Schema.define(version: 20151230031618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,6 +128,7 @@ ActiveRecord::Schema.define(version: 20151111014229) do
     t.integer  "semester_id"
     t.integer  "standing",                  default: 0
     t.integer  "status",                    default: 0
+    t.integer  "sid"
   end
 
   create_table "users", force: :cascade do |t|
