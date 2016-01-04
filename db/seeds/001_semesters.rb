73fb28875e555b5093cf3cfa27d52a4999bad8c4
Howard Chen <howardxchen@gmail.com>
2016-01-04 14:09:56 -0800

    Add days of week to seeds file

--- db/seeds/001_semesters.rb
+++ db/seeds/001_semesters.rb
@@ -1,3 +1,3 @@
-fall_2014 = Semester.where(semester: "Fall", year: 2014).first_or_create!
-spring_2015 = Semester.where(semester: "Spring", year: 2015).first_or_create!
-fall_2015 = Semester.where(semester: "Fall", year: 2015).first_or_create!
+fall_2014 = Semester.where(semester: "Fall", year: 2014, day_of_week: 0).first_or_create!
+spring_2015 = Semester.where(semester: "Spring", year: 2015, day_of_week: 0).first_or_create!
+fall_2015 = Semester.where(semester: "Fall", year: 2015, day_of_week: 0).first_or_create!
