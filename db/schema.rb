e401fbe564e0c6b78ad552a0f8c2f1f12724dae8
Hubert Pham <hubqwerty@berkeley.edu>
2016-02-01 18:06:52 -0800

    added Hubert's profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160105004737) do
+ActiveRecord::Schema.define(version: 20160105071442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "absences", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "excuse"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "approved",   default: 0
+  end
 
   create_table "assignment_submissions", force: :cascade do |t|
     t.integer  "user_id"
