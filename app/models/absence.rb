c1a8e415aeec8e20464ab7b18753ee6c6df40b41
warrenshen <warrenzshen@gmail.com>
2016-06-06 21:32:38 -0700

    Annotate absence model

--- app/models/absence.rb
+++ app/models/absence.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: absences
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  excuse     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  approved   :integer          default(0)
+#
+
 class Absence < ActiveRecord::Base
   enum approved: [ :pending, :accepted, :rejected ]
   belongs_to :user
