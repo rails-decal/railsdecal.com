f9350bc8b6689bfb185f78ccd59669cacf6d0a35
Charles Xue <charlesx2013@gmail.com>
2016-09-06 02:50:41 -0700

    Adding student id

--- app/models/student_application_csv.rb
+++ app/models/student_application_csv.rb
@@ -1,11 +1,11 @@
 class StudentApplicationCsv
-  COLUMN_NAMES = %w(id last_name first_name email status standing)
+  COLUMN_NAMES = %w(id student_id last_name first_name email status standing)
 
   def self.to_csv(student_applications)
     CSV.generate do |csv|
       csv << COLUMN_NAMES
       student_applications.each do |app|
-        csv << [app.id, app.last_name, app.first_name, app.email, app.status, app.standing]
+        csv << [app.id, app.student_id, app.last_name, app.first_name, app.email, app.status, app.standing]
       end
     end
   end
